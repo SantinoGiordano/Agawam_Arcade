dd25bd090a3073a11dbfb7bc3a3d0ef173f19fee
SantinoGiordano <santinogiordano13@gmail.com>
2026-06-08 10:12:36 -0400

    installed font editor

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+// import "@fontsource/press-start-2p";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
